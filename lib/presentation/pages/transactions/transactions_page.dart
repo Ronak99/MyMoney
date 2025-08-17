a53e4691115ccb09ab42b90b2a43c6a1af523431
Ronak99 <punase.ronak99@gmail.com>
2025-08-17 16:04:30 +0530

    linked up dynamic transactions

--- lib/presentation/pages/transactions/transactions_page.dart
+++ lib/presentation/pages/transactions/transactions_page.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_money/model/mock.dart';
 import 'package:my_money/model/transaction.dart';
 import 'package:my_money/presentation/pages/home/widgets/transaction_list_item.dart';
+import 'package:my_money/presentation/pages/transactions/create/state/create_transaction_cubit.dart';
+import 'package:my_money/presentation/pages/transactions/create/state/create_transaction_state.dart';
 import 'package:my_money/presentation/routes/routes.dart';
 import 'package:my_money/presentation/widgets/custom_scaffold.dart';
 import 'package:my_money/presentation/widgets/list_view_separated.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_money/state/transaction/transaction_cubit.dart';
+import 'package:my_money/state/transaction/transaction_state.dart';
 
 class TransactionsPage extends StatelessWidget {
   const TransactionsPage({super.key});
@@ -18,12 +23,15 @@ class TransactionsPage extends StatelessWidget {
         onPressed: () => context.push(Routes.CREATE_TRANSACTION.value),
         child: const Icon(Icons.add),
       ),
-      body: ListViewSeparated<Transaction>(
-        list: Mock.transactions,
-        itemBuilder: (context, _, transaction) {
-          return TransactionListItem(transaction: transaction);
-        },
-      ),
+      body: BlocBuilder<TransactionCubit, TransactionState>(
+          builder: (context, state) {
+        return ListViewSeparated<Transaction>(
+          list: state.transactions,
+          itemBuilder: (context, _, transaction) {
+            return TransactionListItem(transaction: transaction);
+          },
+        );
+      }),
     );
   }
 }
