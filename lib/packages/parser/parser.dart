b7185b888c35ef10968b8b94ec14d0ccf8de5b4d
Ronak99 <punase.ronak99@gmail.com>
2025-08-24 15:27:53 +0530

    chore: import statements

--- lib/packages/parser/parser.dart
+++ lib/packages/parser/parser.dart
@@ -5,10 +5,14 @@ import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:my_money/extensions/list.dart';
+import 'package:my_money/packages/parser/enums/bank.dart';
+import 'package:my_money/packages/parser/exceptions/exceptions.dart';
 import 'package:my_money/packages/parser/model/hdfc_statement_parser.dart';
 import 'package:my_money/packages/parser/model/sbi_statement_parser.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:my_money/model/transaction.dart';
+
+export 'enums/bank.dart';
 
 part 'model/bank_statement_parser.dart';
 part 'model/fi_statement_parser.dart';
