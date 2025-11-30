a30e28c74ebda60662bc15ca854c88913a8adb64
Ronak99 <punase.ronak99@gmail.com>
2025-11-30 19:22:31 +0530

    chore: category wise separation

--- lib/presentation/pages/categories/categories_page.dart
+++ lib/presentation/pages/categories/categories_page.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_money/enums/category_type.dart';
+import 'package:my_money/extensions/categories.dart';
+import 'package:my_money/extensions/string.dart';
+import 'package:my_money/model/account.dart';
 import 'package:my_money/model/transaction_category.dart';
 import 'package:my_money/presentation/widgets/custom_bottom_sheet.dart';
 import 'package:my_money/presentation/widgets/custom_scaffold.dart';
+import 'package:my_money/presentation/widgets/empty_state.dart';
 import 'package:my_money/presentation/widgets/list_item.dart';
+import 'package:my_money/presentation/widgets/list_view_separated.dart';
+import 'package:my_money/presentation/widgets/list_view_with_header.dart';
 import 'package:my_money/state/category/category_cubit.dart';
 import 'package:my_money/state/category/category_state.dart';
-
-import 'package:my_money/model/account.dart';
-import 'package:my_money/presentation/widgets/empty_state.dart';
-import 'package:my_money/presentation/widgets/list_view_separated.dart';
 
 class CategoriesPage extends StatelessWidget {
   const CategoriesPage({super.key});
@@ -22,17 +25,18 @@ class CategoriesPage extends StatelessWidget {
         fab: state.categories.isEmpty
             ? null
             : FloatingActionButton(
-          heroTag: 'categories',
-          onPressed: () =>
-              CustomBottomSheet.modifyCategory().show(context),
-          child: const Icon(Icons.add),
-        ),
+                heroTag: 'categories',
+                onPressed: () =>
+                    CustomBottomSheet.modifyCategory().show(context),
+                child: const Icon(Icons.add),
+              ),
         body: AnimatedCrossFade(
-          firstChild: ListViewSeparated<TransactionCategory>(
-            list: state.categories,
-            itemBuilder: (context, _, category) {
-              return ListItem.category(category);
+          firstChild: ListViewWithHeader<CategoryType, TransactionCategory>(
+            map: state.categories.groupByType,
+            headerBuilder: (CategoryType type) {
+              return Text(type.name.capitalizeFirstLetter);
             },
+            itemBuilder: (category) => ListItem.category(category),
           ),
           secondChild: EmptyState.noCategory(),
           crossFadeState: state.categories.isNotEmpty
